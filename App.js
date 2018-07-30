bab15b5b5f81fa95f889efa7158af885e7d7ec4a
Moe-Mamdouh <mohamed.mamdouh.do@gmail.com>
2018-07-30 21:31:05 +0200

    apply fancy sideMenu animation

--- App.js
+++ App.js
@@ -1,35 +1,28 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- * @flow
- */
+import React, { Component } from 'react';
+import {
+  StyleSheet,
+  Text,
+  View,
+  Image,
+  TouchableOpacity,
+  Animated,
+} from 'react-native';
+import SideMenu from 'react-native-side-menu';
+import Menu from './Menu';
 
-import React, {Component} from 'react';
-import {Platform, StyleSheet, Text, View} from 'react-native';
-
-const instructions = Platform.select({
-  ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
-  android:
-    'Double tap R on your keyboard to reload,\n' +
-    'Shake or press menu button for dev menu',
-});
-
-type Props = {};
-export default class App extends Component<Props> {
-  render() {
-    return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>Welcome to React Native!</Text>
-        <Text style={styles.instructions}>To get started, edit App.js</Text>
-        <Text style={styles.instructions}>{instructions}</Text>
-      </View>
-    );
-  }
-}
+const image = require('./assets/menu.png');
 
 const styles = StyleSheet.create({
+  button: {
+    position: 'absolute',
+    top: 20,
+    padding: 10,
+  },
+  caption: {
+    fontSize: 20,
+    fontWeight: 'bold',
+    alignItems: 'center',
+  },
   container: {
     flex: 1,
     justifyContent: 'center',
@@ -47,3 +40,86 @@ const styles = StyleSheet.create({
     marginBottom: 5,
   },
 });
+
+export default class Basic extends Component {
+  constructor(props) {
+    super(props);
+
+    this.toggle = this.toggle.bind(this);
+
+    this.state = {
+      isOpen: false,
+      selectedItem: 'About',
+    };
+  }
+
+  toggle() {
+    this.setState({
+      isOpen: !this.state.isOpen,
+    });
+  }
+
+  updateMenuState(isOpen) {
+    this.setState({ isOpen });
+  }
+
+  onMenuItemSelected = item =>
+    this.setState({
+      isOpen: false,
+      selectedItem: item,
+    });
+
+  animationStyle = value => {
+    return {
+      transform: [
+        {
+          scale: value.interpolate({
+            inputRange: [0, 100],
+            outputRange: [1, 0.7],
+          }),
+        },
+      ],
+      left: value,
+    };
+  };
+
+  animationFunction = (prop, value) => {
+    return Animated.spring(prop, {
+      toValue: value,
+      friction: 8,
+      tension: 50,
+    });
+  };
+
+  render() {
+    const menu = <Menu onItemSelected={this.onMenuItemSelected} />;
+
+    return (
+      <SideMenu
+        menu={menu}
+        isOpen={this.state.isOpen}
+        openMenuOffset={100}
+        onChange={isOpen => this.updateMenuState(isOpen)}
+        animationFunction={this.animationFunction}
+        animationStyle={this.animationStyle}
+      >
+        <View style={styles.container}>
+          <Text style={styles.welcome}>Welcome to React Native!</Text>
+          <Text style={styles.instructions}>
+            To get started, edit index.ios.js
+          </Text>
+          <Text style={styles.instructions}>
+            Press Cmd+R to reload,{'\n'}
+            Cmd+Control+Z for dev menu
+          </Text>
+          <Text style={styles.instructions}>
+            Current selected menu item is: {this.state.selectedItem}
+          </Text>
+        </View>
+        <TouchableOpacity onPress={this.toggle} style={styles.button}>
+          <Image source={image} style={{ width: 32, height: 32 }} />
+        </TouchableOpacity>
+      </SideMenu>
+    );
+  }
+}
